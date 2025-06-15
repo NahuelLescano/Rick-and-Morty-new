9154d947de737be892cee1b191b8407e0e413e97
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-06-15 18:06:05 -0300

    Renderizo los characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Characters } from "@/characters/Characters";
+import { useCharacterFetch } from "@/characters/hooks/useCharacterFetch";
 
 function App() {
-  const [count, setCount] = useState(0)
+    const { characters } = useCharacterFetch({ url: "https://rickandmortyapi.com/api/character" });
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    return (
+        <>
+            <h2>NavBar</h2>
+            <Characters characters={characters} />
+            <h2>Footer</h2>
+        </>
+    );
 }
 
-export default App
+export default App;
