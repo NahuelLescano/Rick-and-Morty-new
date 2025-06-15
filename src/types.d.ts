7e43624afde86df0ed45cf0b6fb33084cf2e0cab
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-06-15 18:06:52 -0300

    Tipados

---
+++ src/types.d.ts
@@ -0,0 +1,28 @@
+export interface Location {
+    name: string;
+    url: string;
+}
+
+export interface Character {
+    id: number;
+    name: string;
+    status: string;
+    species: string;
+    type: string;
+    gender: string;
+    origin: Location;
+    location: Location;
+    image: string;
+}
+
+export interface Info {
+    count: number;
+    pages: number;
+    next: string | null;
+    prev: string | null;
+}
+
+export interface RickAndMortyApiResponse {
+    info: Info;
+    results: Character[];
+}
