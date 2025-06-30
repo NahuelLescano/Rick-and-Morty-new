995c582d2af7252ba54f3f475e13a8733e22962b
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-06-29 21:50:45 -0300

    Agrego rutas, eso tendria que estar en un archivo aparte

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
-import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Characters } from "@/characters/Characters";
+import { NavBar } from "@/NavBar/NavBar";
+import { Footer } from "@/Footer/Footer";
 import { useCharacterFetch } from "@/characters/hooks/useCharacterFetch";
+import "./App.css";
 
 function App() {
     const { characters } = useCharacterFetch({ url: "https://rickandmortyapi.com/api/character" });
 
     return (
         <>
-            <h2>NavBar</h2>
-            <Characters characters={characters} />
-            <h2>Footer</h2>
+            <NavBar />
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<Characters characters={characters} />} />
+                    <Route path="/about" element={<h1>About Page</h1>} />
+                    <Route path="/favorites" element={<h1>Favorites Page</h1>} />
+                    <Route path="/detail/:id" element={<h1>Detail Page</h1>} />
+                </Routes>
+            </BrowserRouter>
+            <Footer />
         </>
     );
 }
