b847021dce6125dc25397520b7675ea26c5098c1
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-13 23:01:35 -0300

    Footer ahora esta siempre al fondo y no se anda moviendo por ahi

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Characters } from "@/characters/Characters";
 import { NavBar } from "@/NavBar/NavBar";
 import { Footer } from "@/Footer/Footer";
+import { About } from "@/about/About";
 import { useCharacterFetch } from "@/characters/hooks/useCharacterFetch";
 import "./App.css";
 
@@ -9,18 +10,20 @@ function App() {
     const { characters } = useCharacterFetch({ url: "https://rickandmortyapi.com/api/character" });
 
     return (
-        <>
+        <div className="min-h-screen flex flex-col">
             <NavBar />
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<Characters characters={characters} />} />
-                    <Route path="/about" element={<h1>About Page</h1>} />
-                    <Route path="/favorites" element={<h1>Favorites Page</h1>} />
-                    <Route path="/detail/:id" element={<h1>Detail Page</h1>} />
-                </Routes>
-            </BrowserRouter>
+            <main className="flex-1">
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Characters characters={characters} />} />
+                        <Route path="/about" element={<About />} />
+                        <Route path="/favorites" element={<h1>Favorites Page</h1>} />
+                        <Route path="/detail/:id" element={<h1>Detail Page</h1>} />
+                    </Routes>
+                </BrowserRouter>
+            </main>
             <Footer />
-        </>
+        </div>
     );
 }
 
