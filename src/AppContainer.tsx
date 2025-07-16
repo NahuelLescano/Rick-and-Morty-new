844fcc4daa0bafe4355e1fbab3edfcf5c169a1be
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-15 22:56:10 -0300

    Aca tendria que ir logica como error boundries o providers

---
+++ src/AppContainer.tsx
@@ -0,0 +1,10 @@
+import { App } from "./App";
+import { AppRouter } from "./AppRouter";
+
+export const AppContainer = () => {
+    return (
+        <App>
+            <AppRouter />
+        </App>
+    );
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
+import { AppContainer } from "@/AppContainer";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
     <StrictMode>
-        <App />
+        <AppContainer />
     </StrictMode>,
 )
