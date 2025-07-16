316b89675a7100717f2667000f3f5f7d75d82a14
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-15 22:54:39 -0300

    Me gusto el fondo transparente
    Sera demasiado transparente?

--- src/characters/components/CharacterItem.tsx
+++ src/characters/components/CharacterItem.tsx
@@ -1,7 +1,8 @@
 import type { Character } from "@/types.d";
+import { Link } from "react-router-dom";
 
 export const CharacterItem = (
-    { name, status, species, image }: Pick<Character, "name" | "status" | "species" | "image">
+    { id, name, status, species, image }: Pick<Character, "id" | "name" | "status" | "species" | "image">
 ) => {
 
     const getStatusColor = (status: string) => {
@@ -13,18 +14,20 @@ export const CharacterItem = (
     };
 
     return (
-        <div className="mt-8 bg-gray-800 rounded-xl shadow-xl overflow-hidden transition-all duration-300 hover:shadow-2xl hover:scale-105 border border-gray-700">
+        <div className="mt-8 bg-transparent backdrop-blur-md rounded-xl shadow-xl overflow-hidden transition-all duration-300 hover:shadow-2xl hover:scale-105 border border-gray-700">
             <div className="relative">
-                <img 
-                    src={image}
-                    alt={`${name}'s image`}
-                    className="w-full h-66 object-cover"
-                />
+                <Link to={`/detail/${id}`}>
+                    <img
+                        src={image}
+                        alt={`${name}'s image`}
+                        className="w-full h-66 object-cover hover:cursor-pointer"
+                    />
+                </Link>
                 <div className={`absolute top-3 right-3 px-2 py-1 rounded-full text-xs font-semibold text-white ${getStatusColor(status)}`}>
                     {status}
                 </div>
             </div>
-            
+
             <div className="p-4">
                 <h2 className="text-xl font-bold text-white mb-2 truncate">{name}</h2>
                 <div className="space-y-2">
