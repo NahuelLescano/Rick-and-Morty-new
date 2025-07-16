1ac8dd9ae494da10ec97a07fc74c634644496b49
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-15 22:54:01 -0300

    Agrego un 404 para que no se vea horrible si va a una direccion que no existe

---
+++ src/RoutesNotFound/RoutesNotFound.tsx
@@ -0,0 +1,15 @@
+import type { ReactNode } from "react";
+import { Route, Routes } from "react-router-dom";
+
+export const RoutesNotFound = ({ children }: { children: ReactNode }) => {
+    return (
+        <Routes>
+            {children}
+            <Route path="*" element={
+                <h1 className="text-3xl text-center mt-20">
+                    Page Not Found
+                </h1>
+            } />
+        </Routes>
+    );
+}
