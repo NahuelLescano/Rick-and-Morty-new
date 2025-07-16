4bd8d51ce025c1fee559b64d2b47bfc3435bb2ee
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-15 22:54:58 -0300

    Ahora que uso zustand ya no hace falta la costum hook

--- src/characters/Characters.tsx
+++ src/characters/Characters.tsx
@@ -1,15 +1,23 @@
-import type { Character } from "@/types";
 import { CharacterItem } from "./components/CharacterItem";
+import { useCharacterStore } from "@/store/CharacterStore";
+import { useEffect } from "react";
 
-export const Characters = (
-    { characters }: { characters: Character[] }
-) => {
+export const Characters = () => {
+    const characters = useCharacterStore((state) => state.allCharacters);
+    const setAllCharacters = useCharacterStore((state) => state.setAllCharacters);
+
+    useEffect(() => {
+        if (characters.length === 0) {
+            setAllCharacters();
+        }
+    }, []);
     return (
         <main className="grid grid-cols-3 gap-4 z-2 max-w-7xl mx-auto m-0 mt-10 text-lg">
             {
                 characters.map((char) => (
                     <CharacterItem
                         key={char.id}
+                        id={char.id}
                         name={char.name}
                         status={char.status}
                         species={char.species}
