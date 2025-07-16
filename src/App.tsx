57a62dc2784bd7d055cb5aa4ebd83cc78ec32733
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-15 22:55:40 -0300

    Separe la logica de app en AppRouter y AppContainer

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,16 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import { Characters } from "@/characters/Characters";
+import type { ReactNode } from "react";
 import { NavBar } from "@/NavBar/NavBar";
 import { Footer } from "@/Footer/Footer";
-import { About } from "@/about/About";
-import { useCharacterFetch } from "@/characters/hooks/useCharacterFetch";
 import "./App.css";
 
-function App() {
-    const { characters } = useCharacterFetch({ url: "https://rickandmortyapi.com/api/character" });
-
+export const App = ({ children }: { children: ReactNode }) => {
     return (
         <div className="min-h-screen flex flex-col">
             <NavBar />
             <main className="flex-1">
-                <BrowserRouter>
-                    <Routes>
-                        <Route path="/" element={<Characters characters={characters} />} />
-                        <Route path="/about" element={<About />} />
-                        <Route path="/favorites" element={<h1>Favorites Page</h1>} />
-                        <Route path="/detail/:id" element={<h1>Detail Page</h1>} />
-                    </Routes>
-                </BrowserRouter>
+                {children}
             </main>
             <Footer />
         </div>
     );
 }
-
-export default App;
