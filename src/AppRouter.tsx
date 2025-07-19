e2c24c82d727c96c192bf576e79041512955c78d
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-07-18 22:06:48 -0300

    Fix: saco la ruta de favoritos

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter, Route } from "react-router-dom";
 import { Characters } from "@/characters/Characters";
 import { CharacterDetail } from "@/characters/components/CharacterDetail";
-import { About } from "@/about/About";
+import { About } from "@/About/About";
 import { RoutesNotFound } from "./RoutesNotFound/RoutesNotFound";
 
 export const AppRouter = () => {
@@ -10,7 +10,6 @@ export const AppRouter = () => {
             <RoutesNotFound>
                 <Route path="/" element={<Characters />} />
                 <Route path="/about" element={<About />} />
-                <Route path="/favorites" element={<h1>Favorites Page</h1>} />
                 <Route path="/detail/:id" element={<CharacterDetail />} />
             </RoutesNotFound>
         </BrowserRouter>
