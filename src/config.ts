e2cd7ee6debd3c6f57f1ecbe6e89a34f40e09366
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-11-29 21:37:29 -0300

    feat: en caso de no estar definida la url, tira error para que lo crees

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,11 @@
 export const config = {
     API_URL: import.meta.env.VITE_RICK_AND_MORTY_API_URL as string,
-    BASE_URL: import.meta.env.VITE_RICK_AND_MORTY_BASE_URL as string,
+    BASE_URL: import.meta.env.VITE_RICK_AND_MORTY_BASE_URL as string ?? "https://rickandmortyapi.com/api",
 };
+
+if (!config.API_URL) {
+    throw new Error("URL is not defined");
+}
 
 const { API_URL, BASE_URL } = config;
 export { API_URL, BASE_URL };
