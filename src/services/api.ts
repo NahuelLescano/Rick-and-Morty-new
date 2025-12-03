34775e71a66c29c4fce3c556e9a07bd3adcbca3b
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-12-03 20:54:41 -0300

    feat: add new api service call getPage

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,25 +4,34 @@ import type { UseApiCall } from "@/model";
 import type { Character, RickAndMortyApiResponse } from "@/types";
 
 const loadAbort = (): AbortController => {
-    return new AbortController();
+  return new AbortController();
 };
 
 export const getCharacters = (): UseApiCall<RickAndMortyApiResponse> => {
-    const controller = loadAbort();
+  const controller = loadAbort();
 
-    return {
-        call: axios.get(API_URL, { signal: controller.signal }),
-        controller,
-    };
+  return {
+    call: axios.get(API_URL, { signal: controller.signal }),
+    controller,
+  };
 };
 
-export const getCharacterById = (
-    id: number,
-): UseApiCall<Character> => {
-    const controller = loadAbort();
+export const getCharacterById = (id: number): UseApiCall<Character> => {
+  const controller = loadAbort();
 
-    return {
-        call: axios.get(`${API_URL}/${id}`, { signal: controller.signal }),
-        controller,
-    };
+  return {
+    call: axios.get(`${API_URL}/${id}`, { signal: controller.signal }),
+    controller,
+  };
+};
+
+export const getCharactersByPage = (
+  page: number,
+): UseApiCall<RickAndMortyApiResponse> => {
+  const controller = loadAbort();
+
+  return {
+    call: axios.get(`${API_URL}?page=${page}`, { signal: controller.signal }),
+    controller,
+  };
 };
