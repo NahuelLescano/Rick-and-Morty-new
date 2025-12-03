b66ac9807283b17f629fe4295006575931410850
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-12-03 20:11:56 -0300

    feat: add hover

--- src/Globals/NavBar/NavBar.tsx
+++ src/Globals/NavBar/NavBar.tsx
@@ -2,8 +2,8 @@ export const NavBar = () => {
     return (
         <nav className="top-0 w-full fixed p-4 flex justify-between items-center bg-transparent border-b border-white/20 backdrop-blur-md z-50">
             <ul className="flex gap-8">
-                <li><a href="/">Rick and Morty</a></li>
-                <li><a href="/about">About</a></li>
+                <li><a href="/" className="hover:underline">Rick and Morty</a></li>
+                <li><a href="/about" className="hover:underline">About</a></li>
             </ul>
         </nav>
     )
