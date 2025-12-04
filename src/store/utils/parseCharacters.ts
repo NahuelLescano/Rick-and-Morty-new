cf8cb3dd3eccbc89f577950498c4aec1fb590727
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-12-04 00:19:39 -0300

    feat: move away parse characters

---
+++ src/store/utils/parseCharacters.ts
@@ -0,0 +1,25 @@
+import type { RickAndMortyApiResponse, Character } from "@/types";
+
+export const parsedCharacters = (
+  data: RickAndMortyApiResponse,
+): Character[] => {
+  const parsedCharacters: Character[] = data.results.map((char) => ({
+    id: char.id,
+    name: char.name,
+    status: char.status,
+    species: char.species,
+    type: char.type,
+    gender: char.gender,
+    origin: {
+      name: char.origin.name,
+      url: char.origin.url,
+    },
+    location: {
+      name: char.location.name,
+      url: char.location.url,
+    },
+    image: char.image,
+  }));
+
+  return parsedCharacters;
+};
