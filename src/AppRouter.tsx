09493504e85783115ede281ec3e0e9dee66b9b9e
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-12-05 00:35:21 -0300

    feat: use Suspense to pause render when a component is taking time to load

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,16 +1,19 @@
 import { BrowserRouter, Route } from "react-router-dom";
+import { Suspense } from "react";
 import { RoutesNotFound } from "@/RoutesNotFound";
 import { Characters, CharacterDetail } from "@/characters";
-import { About } from "@/Globals";
+import { About, Loading } from "@/Globals";
 
 export const AppRouter = () => {
-  return (
-    <BrowserRouter>
-      <RoutesNotFound>
-        <Route path="/" element={<Characters />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/detail/:id" element={<CharacterDetail />} />
-      </RoutesNotFound>
-    </BrowserRouter>
-  );
+	return (
+		<Suspense fallback={<Loading />}>
+			<BrowserRouter>
+				<RoutesNotFound>
+					<Route path="/" element={<Characters />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/detail/:id" element={<CharacterDetail />} />
+				</RoutesNotFound>
+			</BrowserRouter>
+		</Suspense>
+	);
 };
