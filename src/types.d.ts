8aa4ba8a6c647ddf17ca4af6ec090c431b84d172
Nahuel <lescanoagustin10nahuel@gmail.com>
2025-12-23 20:11:38 -0300

    fix: format

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,28 +1,28 @@
 export interface Location {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 }
 
 export interface Character {
-    id: number;
-    name: string;
-    status: string;
-    species: string;
-    type: string;
-    gender: string;
-    origin: Location;
-    location: Location;
-    image: string;
+  id: number;
+  name: string;
+  status: string;
+  species: string;
+  type: string;
+  gender: string;
+  origin: Location;
+  location: Location;
+  image: string;
 }
 
 export interface Info {
-    count: number;
-    pages: number;
-    next: string | null;
-    prev: string | null;
+  count: number;
+  pages: number;
+  next: string | null;
+  prev: string | null;
 }
 
 export interface RickAndMortyApiResponse {
-    info: Info;
-    results: Character[];
+  info: Info;
+  results: Character[];
 }
