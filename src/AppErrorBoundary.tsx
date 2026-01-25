b2374a347a4e78e4f5028593c5899d3c899e7aab
Nahuel <lescanoagustin10nahuel@gmail.com>
2026-01-25 15:26:56 -0300

    feat: implement stacktrace to better explain errors

--- src/AppErrorBoundary.tsx
+++ src/AppErrorBoundary.tsx
@@ -3,42 +3,50 @@ import { ErrorBoundary, type FallbackProps } from "react-error-boundary";
 import { CommonButton } from "./Globals/CommonButton/CommonButton";
 
 interface ErrorBoundaryProps {
-	children: ReactNode;
-	fallbackComponent?: ComponentType<FallbackProps>;
+  children: ReactNode;
+  fallbackComponent?: ComponentType<FallbackProps>;
 }
 
 export const AppErrorBoundary = ({
-	children,
-	fallbackComponent: FallbackComponent = ErrorFallback,
+  children,
+  fallbackComponent: FallbackComponent = ErrorFallback,
 }: ErrorBoundaryProps) => {
-	return (
-		<ErrorBoundary FallbackComponent={FallbackComponent}>
-			{children}
-		</ErrorBoundary>
-	);
+  return (
+    <ErrorBoundary FallbackComponent={FallbackComponent}>
+      {children}
+    </ErrorBoundary>
+  );
 };
 
 const ErrorFallback = ({ error, resetErrorBoundary }: FallbackProps) => {
-	return (
-		<div className="flex flex-col items-center justify-center min-h-screen p-4">
-			<h2 className="text-2xl font-bold text-red-600 mb-4">Oops! Something went wrong.</h2>
-			<p className="text-gray-700 mb-4">
-				We encountered an unexpected error. Please try again.
-			</p>
-			{error && (
-				<details className="mb-4">
-					<summary className="cursor-pointer text-sm text-white">Error details</summary>
-					<pre className="text-xs text-black bg-transparent p-2 mt-2 rounded overflow-auto">
-						{error.message}
-					</pre>
-				</details>
-			)}
-			<CommonButton
-				onClick={resetErrorBoundary}
-				className="px-4 py-2 rounded hover:bg-blue-600"
-			>
-				Try Again
-			</CommonButton>
-		</div>
-	);
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen p-4">
+      <section className="mb-6 p-6 rounded shadow-md text-center">
+        <h2 className="text-2xl font-bold text-red-600 mb-4">
+          Oops! Something went wrong.
+        </h2>
+        <p className="mb-4 text-white">We encountered an unexpected error.</p>
+        {error && (
+          <details className="mb-4 whitespace-pre-wrap bg-transparent backdrop-blur-md rounded-xl shadow-xl overflow-hidden text-pretty transition-all duration-300 hover:shadow-2xl border border-gray-700 p-4">
+            <summary className="cursor-pointer text-sm text-white">
+              Error details
+            </summary>
+            <pre className="p-2 mt-2 rounded overflow-auto font-semibold text-lg text-white">
+              {error.message}
+            </pre>
+            <pre className="p-2 mt-2 rounded overflow-auto text-white">
+              <h2 className="text-lg text-center font-bold">Stack trace:</h2>
+              {error.stack}
+            </pre>
+          </details>
+        )}
+        <CommonButton
+          onClick={resetErrorBoundary}
+          className="px-4 py-2 rounded hover:bg-blue-600"
+        >
+          Try Again
+        </CommonButton>
+      </section>
+    </div>
+  );
 };
